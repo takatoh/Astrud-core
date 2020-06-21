ee06717a611cffb4f7d05f81fc128d1709599926
takatoh <takatoh.m@gmail.com>
2020-06-21 17:57:02 +0900

    app.py: Route to '/tree'.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from bottle import route, run
 import json
+from pathlib import Path
 
 
 with open("config.json", "r") as f:
@@ -14,6 +15,20 @@ def hello():
 @route("/config")
 def config():
     return json.dumps(Config, indent=2)
+
+
+@route("/tree")
+def tree():
+    tree = dir_tree(Config["photoDir"])
+    return json.dumps(tree, indent=2)
+
+
+def dir_tree(root):
+    p = Path(root)
+    tree = { "name" : root }
+    tree["children"] = [ dir_tree(str(x)) for x in p.iterdir() if x.is_dir() ]
+    return tree
+
 
 
 run(host="localhost", port=8008, debug=True)
