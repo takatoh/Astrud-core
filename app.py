32a454d5bc571fff99e9162cd0b9fa49237a2715
takatoh <takatoh.m@gmail.com>
2020-06-21 17:36:32 +0900

    app.py: Load configuration file.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from bottle import route, run
+import json
+
+
+with open("config.json", "r") as f:
+    Config = json.load(f)
 
 
 @route("/hello")
 def hello():
     return "Hello World!"
 
+
+@route("/config")
+def config():
+    return json.dumps(Config, indent=2)
 
 
 run(host="localhost", port=8008, debug=True)
